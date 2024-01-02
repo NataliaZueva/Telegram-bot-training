1b565cbb561a22d9582fd2ce9a9b9494c351e543
Natalia Zueva <natta_liya@vk.com>
2024-01-03 05:20:06 +0800

    1 com

--- main.py
+++ main.py
@@ -1,88 +1,166 @@
-import logging
-from aiogram import Bot, Dispatcher, executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.callback_data import CallbackData
-import pandas as pd
+import telebot
+from telebot import types
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-bot = Bot(token='5546689740:AAFuKh3kry1Ygfxcs399wgjc1xHMpzX9Ph4')  # создаю бота и указываю его токен
-logging.basicConfig(level=logging.INFO)                            # включаю логирование, чтоб не упустить ошибки
-dp = Dispatcher(bot)                                               # считыватель переписки с ботом (диспетчер)
-data = pd.read_csv('movies_emotions.csv') # костыли
-genre = data['Жанр'].values
-emotion = data['Эмоция'].values
-Callback = CallbackData('filter', 'option', 'action')              # создаю коллбэк для универсальной функции под кнопку
-                                                                   # filter - название функции
-                                                                   # option - выбор пользователя
-                                                                   # action - действие кнопки (Выбор жанра или эмоции)
-
-def DataFrame_to_String(data):                                     # Достаёт из data названия фильмов и помещает все в
-    films = data['Название фильма'].unique()                       # в одну переменную, в разные строки через \n
-    result = ''
-    for film in films:
-        result += film + '\n'
-    return result
+botTimeWeb = telebot.TeleBot('6782691562:AAFOUfac0eiFvfZVH_mCGoa1Oaej0g54DEk')
+chemical_elements = {}  # Пустой словарь химических элементов
+chemical_elements_old = {}  # Пустой словарь химических элементов, которе игрок использовал
+chemical_old = [] # Массив из названий собранных полимеров
 
 
-@dp.callback_query_handler(Callback.filter())                      # Обработка кнопки, создание фильтров
-async def filter_DataFrame_on_emotion(call, callback_data):
-    match callback_data['action']:
-        case 'genre':
-            global genre
-            genre = callback_data['option']
-        case 'emotion':
-            global emotion
-            emotion = callback_data['option']
-
-    await call.message.answer('Вы выбрали - '+str(callback_data['option']))
-
-
-@dp.message_handler(commands=['start', 'help'])                    # Главное меню с информацией
-async def print_hi(message):
-    await message.answer('Приветствую!\n'
-                         'Я бот который поможет тебе выбрать фильм на вечер.'
-                         '\nДля начала тебе нужно выбрать жанр фильм с которыми ты бы хотел видеть.\n'
-                         'Для этого тебе ввести команду /genre и /emotion\n'
-                         'А когда определишься со всеми жанрами и эмоциями введи команду /show')  # genre emotion
+# начальное окно спрашивает о действиях после команды /start
+@botTimeWeb.message_handler(commands=['start'])
+def startBot(message):
+    markup = types.InlineKeyboardMarkup()
+    button_begin = types.InlineKeyboardButton(text='Начать игру', callback_data='game_begin')
+    button_rules = types.InlineKeyboardButton(text='Правила игры', callback_data='game_rules')
+    button_exit = types.InlineKeyboardButton(text='Выйти', callback_data='exit')
+    markup.row(button_begin)
+    markup.row(button_rules, button_exit)
+    botTimeWeb.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
 
 
-@dp.message_handler(commands='genre')                              # Выбор жанра
-async def genre_filter(message):
-    keyboard_of_genres = InlineKeyboardMarkup()                    # Объявление клавиатуры
-    buttons = [InlineKeyboardButton(text='Драма', callback_data=Callback.new(option='Драма', action='genre')),
-               InlineKeyboardButton(text='Боевик', callback_data=Callback.new(option='Боевик', action='genre')),
-               InlineKeyboardButton(text='Фантастика', callback_data=Callback.new(option='Фантастика', action='genre')),
-               InlineKeyboardButton(text='Приключения', callback_data=Callback.new(option='Приключения', action='genre')),
-               InlineKeyboardButton(text='Комедия', callback_data=Callback.new(option='Комедия', action='genre')),
-               InlineKeyboardButton(text='Криминал', callback_data=Callback.new(option='Криминал', action='genre')),
-               InlineKeyboardButton(text='Семейный', callback_data=Callback.new(option='Семейный', action='genre')),
-               InlineKeyboardButton(text='Фэнтази', callback_data=Callback.new(option='Фэнтази', action='genre')),
-               InlineKeyboardButton(text='Ужасы', callback_data=Callback.new(option='Ужасы', action='genre'))]                                                # Создание кнопок в массиве (кидают на filter_DataFrame_on_emotion)
-    keyboard_of_genres.add(*buttons)                               # Добавление всех кнопок в клавиатуру
-    await message.answer('Выбери жанр:', reply_markup=keyboard_of_genres)
+# Правила игры
+@botTimeWeb.callback_query_handler(func=lambda call: call.data == 'game_rules')
+def gameRules(call):
+    botTimeWeb.send_message(call.message.chat.id, "Вот правила игры:")
+    botTimeWeb.send_photo(call.message.chat.id, photo=open('khimloto.png', 'rb'))
+    botTimeWeb.send_message(call.message.chat.id, "Чтобы вернуться назад, нажмите /start")
 
 
-@dp.message_handler(commands='emotion')                            # Выбор эмоции
-async def genre_filter(message):
-    keyboard_of_emotions = InlineKeyboardMarkup()
-    buttons = [InlineKeyboardButton(text='Странный', callback_data=Callback.new(option='Странный', action='emotion')),
-               InlineKeyboardButton(text='Волнительный', callback_data=Callback.new(option='Волнительный', action='emotion')),
-               InlineKeyboardButton(text='Вдохновляющий', callback_data=Callback.new(option='Вдохновляющий', action='emotion')),
-               InlineKeyboardButton(text='Грустный', callback_data=Callback.new(option='Грустный', action='emotion')),
-               InlineKeyboardButton(text='Добрый', callback_data=Callback.new(option='Добрый', action='emotion')),
-               InlineKeyboardButton(text='Глупый', callback_data=Callback.new(option='Глупый', action='emotion')),
-               InlineKeyboardButton(text='Страшный', callback_data=Callback.new(option='Страшный', action='emotion')),
-               InlineKeyboardButton(text='Трогательный', callback_data=Callback.new(option='Трогательный', action='emotion')),
-               InlineKeyboardButton(text='Страшный', callback_data=Callback.new(option='Страшный', action='emotion'))]
-    keyboard_of_emotions.add(*buttons)
-    await message.answer('Выбери жанр:', reply_markup=keyboard_of_emotions)
+# После начала начинается игра с кнопками
+@botTimeWeb.callback_query_handler(func=lambda call: call.data == 'game_begin')
+def gameBegin(call):
+    markup = types.ReplyKeyboardMarkup(row_width=5)
+    button_c = types.KeyboardButton(text='C')
+    button_h = types.KeyboardButton(text='H')
+    button_o = types.KeyboardButton(text='O')
+    button_n = types.KeyboardButton(text='N')
+    button_cl = types.KeyboardButton(text='Cl')
+    markup.add(button_c, button_h, button_o, button_n, button_cl)
+
+    button_ch = types.KeyboardButton(text='CH')
+    button_ch2 = types.KeyboardButton(text='CH2')
+    button_ch3 = types.KeyboardButton(text='CH3')
+    button_c6h4 = types.KeyboardButton(text='C6H4')
+    button_c6h5 = types.KeyboardButton(text='C6H5')
+    markup.add(button_ch, button_ch2, button_ch3, button_c6h4, button_c6h5)
+
+    button_rules = types.KeyboardButton(text='Правила')
+    button_polymer = types.KeyboardButton(text='Полимеры ')
+    markup.add(button_rules, button_polymer)
+
+    button_used_sf = types.KeyboardButton(text='Использованные СФ')
+    button_finish = types.KeyboardButton(text='Закончить')
+    markup.add(button_used_sf, button_finish)
+
+    botTimeWeb.send_message(call.message.chat.id, "Выберите действие:", reply_markup=markup)
 
 
-@dp.message_handler(commands='show')
-async def show_DataFrame(message):                                 # Показывает результат
-    filted_DataFrame = data[(data['Жанр'] == genre) & (data['Эмоция'] == emotion)] # Фильтрует датафрейм по фильтрам, что мы выставили
-                                                                                   # если мы не выберали, он всё оставит без фильтрации
-    await message.answer(DataFrame_to_String(filted_DataFrame))                    # Отправляет филтрованный датафрейм, преобразованный в строку
+# проверка нажатия на кнопки хим элементов
+@botTimeWeb.message_handler(
+    func=lambda message: message.text in ['C', 'H', 'O', 'N', 'Cl', 'CH', 'CH2', 'CH3', 'C6H4', 'C6H5'])
+def add_element(message):
+    element = message.text
+    if element in chemical_elements:
+        chemical_elements[element] += 1
+    else:
+        chemical_elements[element] = 1
+
+    elements = '\n'.join([f"{key}: {value}" for key, value in chemical_elements.items()])
+    botTimeWeb.send_message(message.chat.id, f"Элементы, которые есть на данный момент:\n{elements}")
+
+    # Проверка наличия достаточного количества элементов для полимеров
+    if check_polymer():
+        polymers = get_ready_polymers()
+        poly = []
+        for polymer, elements in polymers.items():
+            elements_str = ', '.join([f"{key}: {value}" for key, value in elements.items()])
+            poly.append(f"Название: {polymer}, его состав: {elements_str}")
+        poly_message = "\n".join(poly)
+        botTimeWeb.send_message(message.chat.id, f"Можно собрать следующие полимеры: \n{poly_message}")
+
+        # создание кнопок Да и Нет для выбора
+        markup = types.InlineKeyboardMarkup()
+        button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+        button_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+        markup.row(button_yes, button_no)
+        botTimeWeb.send_message(message.chat.id, "Хотите собрать что-нибудь??", reply_markup=markup)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)                  # Запускает проверку сообщений в диалоге (запускает бота)
+@botTimeWeb.callback_query_handler(func=lambda call: True)
+def callback_handler(callback_query):
+    handle_button_click(callback_query)
+
+
+def handle_button_click(callback_query):
+    if callback_query.data == 'yes':
+        polymers = get_ready_polymers()
+        if polymers:
+            buttons = []
+            for polymer, elements in polymers.items():
+                button_text = f"{polymer}"
+                button = InlineKeyboardButton(button_text, callback_data=polymer)
+                buttons.append(button)
+            reply_markup = InlineKeyboardMarkup([buttons])
+            botTimeWeb.send_message(callback_query.message.chat.id,
+                                    "Соберите следующие полимеры прямо сейчас:",
+                                    reply_markup=reply_markup)
+
+
+def check_polymer():
+    polymers = {
+        'glycine': {'O': 2, 'H': 5, 'N': 1, 'C': 2},
+        'kvskm': {'O': 2},
+        'sgrthe': {'N': 2}
+    }
+
+    for polymer, elements in polymers.items():
+        if all(element in chemical_elements and chemical_elements[element] >= count for element, count in
+               elements.items()):
+            return True
+
+    return False
+
+
+def get_ready_polymers():
+    polymers = {
+        'glycine': {'O': 2, 'H': 5, 'N': 1, 'C': 2},
+        'kvskm': {'O': 2}
+    }
+
+    ready_polymers = {}
+    for polymer, elements in polymers.items():
+        if all(element in chemical_elements and chemical_elements[element] >= count for element, count in
+               elements.items()):
+            ready_polymers[polymer] = elements
+
+    return ready_polymers
+
+
+def get_confirmation_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    button_yes = types.KeyboardButton(text='Да')
+    button_no = types.KeyboardButton(text='Нет')
+    markup.add(button_yes, button_no)
+    return markup
+
+
+@botTimeWeb.message_handler(func=lambda message: message.text in ['Да', 'Нет'])
+def handle_confirmation(message):
+    if message.text == 'Да':
+        for element, count in chemical_elements.items():
+            if element in chemical_elements_old:
+                chemical_elements_old[element] += count
+            else:
+                chemical_elements_old[element] = count
+        chemical_elements.clear()
+        botTimeWeb.send_message(message.chat.id, "Химические элементы добавлены в использованные.")
+    else:
+        botTimeWeb.send_message(message.chat.id, "Химические элементы не добавлены в использованные.")
+
+    elements = '\n'.join([f"{key}: {value}" for key, value in chemical_elements.items()])
+    botTimeWeb.send_message(message.chat.id, f"Элементы, которые есть на данный момент:\n{elements}")
+
+
+botTimeWeb.polling()
